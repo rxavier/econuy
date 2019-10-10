3c332badf9bf97af92e9bcccf8fee5c0003a01ee
Rafael Xavier <rxaviermontero@gmail.com>
2019-10-10 16:29:55 -0300

    Change default arg to None

--- retrieval/cpi.py
+++ retrieval/cpi.py
@@ -3,7 +3,7 @@ import pandas as pd
 from processing import colnames
 
 
-def get(update=False, revise=0, save=False):
+def get(update=None, revise=0, save=None):
 
     file = "http://ine.gub.uy/c/document_library/get_file?uuid=2e92084a-94ec-4fec-b5ca-42b40d5d2826&groupId=10181"
 
@@ -13,7 +13,7 @@ def get(update=False, revise=0, save=False):
     cpi.columns = ["CPI index"]
     cpi = cpi.apply(pd.to_numeric, errors="coerce")
 
-    if update is not False:
+    if update is not None:
         previous_data = pd.read_csv(update, sep=" ", index_col=0, header=[0, 1, 2, 3, 4, 5, 6, 7, 8])
         previous_data.index = pd.to_datetime(previous_data.index)
         non_revised = previous_data[:len(previous_data)-revise]
@@ -24,7 +24,7 @@ def get(update=False, revise=0, save=False):
     colnames.set_colnames(cpi, area="Prices and wages", currency="-", inf_adj="No",
                           index="2010-10-31", seas_adj="NSA", ts_type="-", cumperiods=1)
 
-    if save is not False:
+    if save is not None:
         cpi.to_csv(save, sep=" ")
 
     return cpi
