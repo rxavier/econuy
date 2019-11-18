8a4da1f7a2a04e5dd12f96f7735680a82fa3e4f3
Rafael Xavier <rxaviermontero@gmail.com>
2019-11-18 18:13:53 -0300

    Calculate rolling periods matching base df

--- processing/convert.py
+++ processing/convert.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from retrieval import nxr, cpi
-from processing import freqs, colnames
+from processing import freqs, colnames, rolling
 
 
 def usd(df):
@@ -13,14 +13,17 @@ def usd(df):
     if df.columns.get_level_values("Type")[0] == "Flow":
 
         colnames.set_colnames(nxr_data, ts_type="Flow")
-        nxr_matching_freq = freqs.freq_resample(nxr_data, target=inferred_freq, operation="average").iloc[:, 1]
+        nxr_matching_freq = freqs.freq_resample(nxr_data, target=inferred_freq, operation="average").iloc[:, [1]]
+
+        cum_periods = int(df.columns.get_level_values("Cumulative periods")[0])
+        nxr_matching_freq = rolling.rolling(nxr_matching_freq, periods=cum_periods, operation="average")
 
     else:
 
         colnames.set_colnames(nxr_data, ts_type="Stock")
-        nxr_matching_freq = freqs.freq_resample(nxr_data, target=inferred_freq, operation="average").iloc[:, 3]
+        nxr_matching_freq = freqs.freq_resample(nxr_data, target=inferred_freq, operation="average").iloc[:, [3]]
 
-    nxr_to_use = nxr_matching_freq[nxr_matching_freq.index.isin(df.index)]
+    nxr_to_use = nxr_matching_freq[nxr_matching_freq.index.isin(df.index)].iloc[:, 0]
     converted_df = df.apply(lambda x: x / nxr_to_use)
 
     colnames.set_colnames(converted_df, currency="USD")
@@ -34,8 +37,12 @@ def real(df, start_date=None, end_date=None):
 
     cpi_data = cpi.get()
     colnames.set_colnames(cpi_data, ts_type="Flow")
-    cpi_matching_freq = freqs.freq_resample(cpi_data, target=inferred_freq, operation="average").iloc[:, 0]
-    cpi_to_use = cpi_matching_freq[cpi_matching_freq.index.isin(df.index)]
+    cpi_matching_freq = freqs.freq_resample(cpi_data, target=inferred_freq, operation="average").iloc[:, [0]]
+
+    cum_periods = int(df.columns.get_level_values("Cumulative periods")[0])
+    cpi_matching_freq = rolling.rolling(cpi_matching_freq, periods=cum_periods, operation="average")
+
+    cpi_to_use = cpi_matching_freq[cpi_matching_freq.index.isin(df.index)].iloc[:, 0]
 
     if start_date is None:
         converted_df = df.apply(lambda x: x / cpi_to_use)
