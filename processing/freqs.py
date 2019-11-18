191d82d56f865b2f782c117554f2864fcfff2149
Rafael Xavier <rxaviermontero@gmail.com>
2019-11-18 18:12:54 -0300

    Change cumulative periods when resampling

--- processing/freqs.py
+++ processing/freqs.py
@@ -15,6 +15,13 @@ def freq_resample(df, target, operation="sum", interpolation="linear"):
             raise Warning("Only sum, average and upsample are accepted operations")
             return
 
+        cum_periods = df.columns.get_level_values("Cumulative periods")[0]
+        if cum_periods != 1:
+            input_notna = df.iloc[:, 0].count()
+            output_notna = resampled_df.iloc[:, 0].count()
+            cum_adj = round(output_notna/input_notna)
+            colnames.set_colnames(resampled_df, cumperiods=int(cum_periods*cum_adj))
+
     elif df.columns.get_level_values("Type")[0] == "Stock":
 
         resampled_df = df.resample(target, convention="end").asfreq().interpolate(method=interpolation)
