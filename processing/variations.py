9b9c29f0cf10b8b5d0f511da44e17844a9a34f3a
Rafael Xavier <rxaviermontero@gmail.com>
2020-01-23 00:05:09 -0300

    Change operation options and function name

--- processing/variations.py
+++ processing/variations.py
@@ -3,7 +3,8 @@ import pandas as pd
 from processing import colnames, freqs
 
 
-def var_diff(df, operation="var", period_op="last"):
+def chg_diff(df: pd.DataFrame, operation: str = "chg",
+             period_op: str = "last"):
     """Wrapper for the pct_change and diff Pandas methods.
 
     Calculate percentage change or difference for dataframes. The `period`
@@ -37,14 +38,20 @@ def var_diff(df, operation="var", period_op="last"):
     inferred_freq = pd.infer_freq(df.index)
 
     type_change = {"last":
-                   {"var": [lambda x: x.pct_change(periods=1), "% variación"],
-                    "diff": [lambda x: x.diff(periods=1, min_periods=1), "Cambio"]},
+                   {"chg": [lambda x: x.pct_change(periods=1),
+                            "% variación"],
+                    "diff": [lambda x: x.diff(periods=1, min_periods=1),
+                             "Cambio"]},
                    "inter":
-                   {"var": [lambda x: x.pct_change(periods=last_year), "% variación interanual"],
-                    "diff": [lambda x: x.diff(periods=last_year), "Cambio interanual"]},
+                   {"chg": [lambda x: x.pct_change(periods=last_year),
+                            "% variación interanual"],
+                    "diff": [lambda x: x.diff(periods=last_year),
+                             "Cambio interanual"]},
                    "annual":
-                   {"var": [lambda x: x.pct_change(periods=last_year), "% variación anual"],
-                    "diff": [lambda x: x.diff(periods=last_year), "Cambio anual"]}}
+                   {"chg": [lambda x: x.pct_change(periods=last_year),
+                            "% variación anual"],
+                    "diff": [lambda x: x.diff(periods=last_year),
+                             "Cambio anual"]}}
 
     if inferred_freq == "M":
         last_year = 12
@@ -53,7 +60,8 @@ def var_diff(df, operation="var", period_op="last"):
     elif inferred_freq == "A":
         last_year = 1
     else:
-        raise ValueError("The dataframe needs to have a frequency of M (month end), Q (quarter end) or A (year end)")
+        raise ValueError("The dataframe needs to have a frequency of M "
+                         "(month end), Q (quarter end) or A (year end)")
 
     if period_op == "annual":
 
