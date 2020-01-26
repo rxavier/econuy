e87243578c2b281f48fe57ccccfa2d317c8d0f1e
Rafael Xavier <rxaviermontero@gmail.com>
2020-01-26 20:57:28 -0300

    Docs, types and lots of refactoring in nat accounts

--- retrieval/national_accounts.py
+++ retrieval/national_accounts.py
@@ -1,5 +1,7 @@
 import os
 import datetime as dt
+from pathlib import Path
+from typing import Union
 
 import pandas as pd
 from pandas.tseries.offsets import MonthEnd
@@ -48,9 +50,27 @@ FILES = {"https://www.bcu.gub.uy/Estadisticas-e-Indicadores/Cuentas%20Nacionales
              {"Rows": 2, "Inf. Adj.": "Corriente", "Index": "No", "Seas": "NSA", "Name": "na_gdp_cur_nsa",
               "Colnames": ["PBI"]}}
 
+def get(update: bool = False, revise_rows: int = 0,
+        save: bool = False, force_update: bool = False):
+    """Get national accounts data.
 
-def get(update=False, revise_rows=0, save=False, force_update=False):
+    Parameters
+    ----------
+    update : bool (default is False)
+        If true, try to update existing data on disk.
+    revise_rows : int (default is 0)
+        How many rows of old data to replace with new data.
+    save : bool (default is False)
+        If true, save output dataframe in CSV format.
+    force_update : bool (default is False)
+        If True, fetch data and update existing data even if it was modified
+        within its update window (for national accounts data, 80 days)
 
+    Returns
+    -------
+    dataframe : Pandas dataframe
+
+    """
     parsed_excels = {}
     for file, metadata in FILES.items():
 
@@ -59,40 +79,53 @@ def get(update=False, revise_rows=0, save=False, force_update=False):
             delta, previous_data = updates.check_modified(update_path)
 
             if delta < update_threshold and force_update is False:
-                print(f"{metadata['Name']}.csv was modified within {update_threshold} day(s). Skipping download...")
+                print(f"{metadata['Name']}.csv was modified within"
+                      f" {update_threshold} day(s). Skipping download...")
                 parsed_excels.update({metadata["Name"]: previous_data})
                 continue
 
-        base = pd.read_excel(file, skiprows=9, nrows=metadata["Rows"])
-        base_pruned = base.drop(columns=["Unnamed: 0"]).dropna(axis=0, how="all").dropna(axis=1, how="all")
-        base_transpose = base_pruned.transpose()
-        base_transpose.columns = metadata["Colnames"]
-        base_transpose.drop(["Unnamed: 1"], inplace=True)
+        raw = pd.read_excel(file, skiprows=9, nrows=metadata["Rows"])
+        proc = (raw.drop(columns=["Unnamed: 0"]).
+                       dropna(axis=0, how="all").dropna(axis=1, how="all"))
+        proc = proc.transpose()
+        proc.columns = metadata["Colnames"]
+        proc.drop(["Unnamed: 1"], inplace=True)
 
-        fix_na_dates(base_transpose)
+        fix_na_dates(proc)
 
         if metadata["Index"] == "No":
-            base_transpose = base_transpose.divide(1000)
-
+            proc = proc.divide(1000)
         if update is True:
-            base_transpose = updates.revise(new_data=base_transpose, prev_data=previous_data, revise_rows=revise_rows)
+            proc = updates.revise(new_data=proc, prev_data=previous_data,
+                                  revise_rows=revise_rows)
+        proc = proc.apply(pd.to_numeric, errors="coerce")
 
-        base_transpose = base_transpose.apply(pd.to_numeric, errors="coerce")
-
-        columns.set_metadata(base_transpose, area="Actividad económica", currency="UYU", inf_adj=metadata["Inf. Adj."],
-                             index=metadata["Index"], seas_adj=metadata["Seas"], ts_type="Flujo", cumperiods=1)
+        columns.set_metadata(proc, area="Actividad económica", currency="UYU",
+                             inf_adj=metadata["Inf. Adj."],
+                             index=metadata["Index"], seas_adj=metadata["Seas"],
+                             ts_type="Flujo", cumperiods=1)
 
         if save is True:
             save_path = os.path.join(DATA_PATH, metadata['Name'] + ".csv")
-            base_transpose.to_csv(save_path, sep=" ")
+            proc.to_csv(save_path, sep=" ")
 
-        parsed_excels.update({metadata["Name"]: base_transpose})
+        parsed_excels.update({metadata["Name"]: proc})
 
     return parsed_excels
 
 
 def fix_na_dates(df):
+    """Cleanup dates inplace in BCU national accounts files.
 
+    Parameters
+    ----------
+    df : Pandas dataframe
+
+    Returns
+    -------
+    None
+
+    """
     df.index = df.index.str.replace("*", "")
     df.index = df.index.str.replace(r"\bI \b", "3-", regex=True)
     df.index = df.index.str.replace(r"\bII \b", "6-", regex=True)
@@ -101,17 +134,43 @@ def fix_na_dates(df):
     df.index = pd.to_datetime(df.index, format="%m-%Y") + MonthEnd(1)
 
 
-def lin_gdp(update=None, save=None, force_update=False):
+def lin_gdp(update: Union[str, Path, None] = None,
+            save: Union[str, Path, None] = None,
+            force_update: bool = False):
+    """Get nominal GDP data in UYU and USD with forecasts.
 
+    Update nominal GDP data for use in the `convert.pcgdp()` function.
+    Get IMF forecasts for year of last available data point and the next
+    year (for example, if the last period available at the BCU website is
+    september 2019, fetch forecasts for 2019 and 2020).
+
+    Parameters
+    ----------
+    update : str, Path or None (default is None)
+        Path or path-like string pointing to a CSV file for updating.
+    save : str, Path or None (default is None)
+        Path or path-like string where to save the output dataframe in CSV
+        format.
+    force_update : bool (default is False)
+        If True, fetch data and update existing data even if it was modified
+        within its update window (for national accounts data, 80 days)
+
+    Returns
+    -------
+    dataframe : Pandas dataframe
+
+    """
     if update is not None:
         update_path = os.path.join(DATA_PATH, update)
         delta, previous_data = updates.check_modified(update_path)
 
         if delta < update_threshold and force_update is False:
-            print(f"{update} was modified within {update_threshold} day(s). Skipping download...")
+            print(f"{update} was modified within {update_threshold} day(s). "
+                  f"Skipping download...")
             return previous_data
 
-    data_uyu = get(update=True, revise_rows=4, save=True, force_update=False)["na_gdp_cur_nsa"]
+    data_uyu = get(update=True, revise_rows=4, save=True,
+                   force_update=False)["na_gdp_cur_nsa"]
     data_uyu = freqs.rolling(data_uyu, periods=4, operation="sum")
     data_usd = convert.usd(data_uyu)
 
@@ -123,15 +182,22 @@ def lin_gdp(update=None, save=None, force_update=False):
     results = []
     for table, gdp in zip(["NGDP", "NGDPD"], data):
 
-        table_url = (f"https://www.imf.org/external/pubs/ft/weo/2019/02/weodata/weorept.aspx?sy={last_year-1}&ey="
-                     f"{last_year+1}&scsm=1&ssd=1&sort=country&ds=.&br=1&pr1.x=27&pr1.y=9&c=298&s={table}&grp=0&a=")
-        imf_data = pd.to_numeric(pd.read_html(table_url)[4].iloc[2, [5, 6, 7]].reset_index(drop=True))
-        forecast = (gdp.loc[[dt.datetime(last_year-1, 12, 31)]].multiply(imf_data.iloc[1]).divide(imf_data.iloc[0]).
-                    rename(index={dt.datetime(last_year-1, 12, 31): dt.datetime(last_year, 12, 31)}))
-        forecast = forecast.append(gdp.loc[[dt.datetime(last_year-1, 12, 31)]].multiply(imf_data.iloc[2]).
-                                   divide(imf_data.iloc[0]).rename(index={dt.datetime(last_year-1, 12, 31):
-                                                                          dt.datetime(last_year+1, 12, 31)}))
-        gdp = gdp.append(forecast)
+        table_url = (f"https://www.imf.org/external/pubs/ft/weo/2019/02/weodat"
+                     f"a/weorept.aspx?sy={last_year-1}&ey={last_year+1}&scsm=1"
+                     f"&ssd=1&sort=country&ds=.&br=1&pr1.x=27&pr1.y=9&c=298&s"
+                     f"={table}&grp=0&a=")
+        imf_data = pd.to_numeric(pd.read_html(table_url)[4].iloc[2, [5, 6, 7]])
+        imf_data = imf_data.reset_index(drop=True)
+        fcast = (gdp.loc[[dt.datetime(last_year-1, 12, 31)]].
+                 multiply(imf_data.iloc[1]).divide(imf_data.iloc[0]))
+        fcast = fcast.rename(index={dt.datetime(last_year-1, 12, 31):
+                                    dt.datetime(last_year, 12, 31)})
+        next_fcast = (gdp.loc[[dt.datetime(last_year-1, 12, 31)]].
+                      multiply(imf_data.iloc[2]).divide(imf_data.iloc[0]))
+        next_fcast = next_fcast.rename(index={dt.datetime(last_year-1, 12, 31):
+                                              dt.datetime(last_year+1, 12, 31)})
+        fcast = fcast.append(next_fcast)
+        gdp = gdp.append(fcast)
         results.append(gdp)
 
     output = pd.concat(results, axis=1)
