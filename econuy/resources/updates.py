c6cf497a69842f8d17eb90d1240d3381abd8afde
Rafael Xavier <rxaviermontero@gmail.com>
2020-02-02 01:50:58 -0300

    check_modified tells user file doesn't exist and won't will be updated

--- econuy/resources/updates.py
+++ econuy/resources/updates.py
@@ -6,18 +6,29 @@ from pathlib import Path
 import pandas as pd
 
 
-def check_modified(data_path: Union[str, Path]):
+def check_modified(data_path: Union[str, PathLike], multiindex=True):
     """Check how long it has been since the file in path was modified."""
-    modified_time = dt.datetime.fromtimestamp(os.path.getmtime(data_path))
-    delta = (dt.datetime.now() - modified_time).days
-    previous_data = pd.read_csv(data_path, sep=" ", index_col=0, header=[0, 1, 2, 3, 4, 5, 6, 7, 8])
-    previous_data.index = pd.to_datetime(previous_data.index)
+    try:
+        modified_time = dt.datetime.fromtimestamp(path.getmtime(data_path))
+        delta = (dt.datetime.now() - modified_time).days
+        if multiindex is True:
+            previous_data = pd.read_csv(data_path, index_col=0,
+                                        header=list(range(9)))
+        else:
+            previous_data = pd.read_csv(data_path, index_col=0)
+        previous_data.index = pd.to_datetime(previous_data.index)
+    except FileNotFoundError:
+        print(f"{data_path} does not exist. No data will be updated")
+        delta = 0
+        previous_data = pd.DataFrame()
 
     return delta, previous_data
 
 
 def revise(new_data: pd.DataFrame, prev_data: pd.DataFrame, revise_rows: int):
     """Replace n rows of data at the end of a dataframe with new data."""
+    if len(prev_data) == 0:
+        return new_data
     non_revised = prev_data[:len(prev_data)-revise_rows]
     revised = new_data[len(prev_data)-revise_rows:]
     non_revised.columns = new_data.columns
