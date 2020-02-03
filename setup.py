4e06b0347e444f7f70d23f2558c38e3c67e6412b
Rafael Xavier <rxaviermontero@gmail.com>
2020-02-03 11:29:29 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="econuy",
-    version="0.2.2",
+    version="0.2.3",
     description="Download and process Uruguayan economic data.",
     long_description=long_description,
     long_description_content_type='text/markdown',
