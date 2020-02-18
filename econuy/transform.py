f73de12222c9560a5f83eff8197a930a1acf6175
Rafael Xavier <rxaviermontero@gmail.com>
2020-02-17 21:31:46 -0300

    Fix stuff that came up in testing

--- econuy/transform.py
+++ econuy/transform.py
@@ -285,6 +285,8 @@ def rolling(df: pd.DataFrame, periods: Optional[int] = None,
 
     """
     pd_frequencies = {"A": 1,
+                      "A-DEC": 1,
+                      "Q": 4,
                       "Q-DEC": 4,
                       "M": 12}
 
@@ -531,7 +533,7 @@ def chg_diff(df: pd.DataFrame, operation: str = "chg",
     type_change = {"last":
                    {"chg": [lambda x: x.pct_change(periods=1),
                             "% variación"],
-                    "diff": [lambda x: x.diff(periods=1, min_periods=1),
+                    "diff": [lambda x: x.diff(periods=1),
                              "Cambio"]},
                    "inter":
                    {"chg": [lambda x: x.pct_change(periods=last_year),
@@ -548,7 +550,7 @@ def chg_diff(df: pd.DataFrame, operation: str = "chg",
         last_year = 12
     elif inferred_freq == "Q" or inferred_freq == "Q-DEC":
         last_year = 4
-    elif inferred_freq == "A":
+    elif inferred_freq == "A" or inferred_freq == "A-DEC":
         last_year = 1
     else:
         raise ValueError("The dataframe needs to have a frequency of M "
@@ -557,17 +559,19 @@ def chg_diff(df: pd.DataFrame, operation: str = "chg",
     if period_op == "annual":
 
         if df.columns.get_level_values("Tipo")[0] == "Stock":
-            output = df.apply(type_change[period_op][operation][0]).multiply(
-                100)
+            output = df.apply(type_change[period_op][operation][0])
         else:
             output = rolling(df, operation="sum")
             output = output.apply(
-                type_change[period_op][operation][0]).multiply(100)
+                type_change[period_op][operation][0])
 
         columns._setmeta(output, index=type_change[period_op][operation][1])
 
     else:
-        output = df.apply(type_change[period_op][operation][0]).multiply(100)
+        output = df.apply(type_change[period_op][operation][0])
         columns._setmeta(output, index=type_change[period_op][operation][1])
+
+    if operation == "chg":
+        output = output.multiply(100)
 
     return output
