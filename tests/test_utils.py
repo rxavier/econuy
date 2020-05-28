ecd5231da7e92c0449a83bc808acf28c51b4dbd2
Rafael Xavier <rxaviermontero@gmail.com>
2020-05-27 23:34:01 -0300

    Update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,22 +1,29 @@
+from os import path
+
 import pytest
+from sqlalchemy import create_engine
 
 from econuy.retrieval import cpi
-from .test_session import remove_clutter
+try:
+    from tests.test_session import remove_clutter
+except ImportError:
+    from .test_session import remove_clutter
+
+
+CUR_DIR = path.abspath(path.dirname(__file__))
+TEST_DIR = path.join(path.dirname(CUR_DIR), "test-data")
+TEST_CON = create_engine("sqlite://").connect()
 
 
 def test_revise():
     remove_clutter()
-    prices = cpi.get(save_loc="test-data")
-    prices_alt = cpi.get(update_loc="test-data", force_update=True,
-                         revise_rows=6)
+    prices = cpi.get(save_loc=TEST_DIR)
+    prices_alt = cpi.get(update_loc=TEST_DIR, revise_rows=6)
     assert prices.round(4).equals(prices_alt.round(4))
-    prices_alt = cpi.get(update_loc="test-data", force_update=True,
-                         revise_rows="nodup")
+    prices_alt = cpi.get(update_loc=TEST_DIR, revise_rows="nodup")
     assert prices.round(4).equals(prices_alt.round(4))
-    prices_alt = cpi.get(update_loc="test-data", force_update=True,
-                         revise_rows="auto")
+    prices_alt = cpi.get(update_loc=TEST_DIR, revise_rows="auto")
     assert prices.round(4).equals(prices_alt.round(4))
     with pytest.raises(ValueError):
-        cpi.get(update_loc="test-data", force_update=True,
-                revise_rows="wrong")
+        cpi.get(update_loc=TEST_DIR, revise_rows="wrong")
     remove_clutter()
