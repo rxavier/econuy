897e5c65edcc9c94606d77e58d7b831f08ad7ee9
Rafael Xavier <rxaviermontero@gmail.com>
2020-05-28 12:09:58 -0300

    Add SQL testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ import pytest
 from sqlalchemy import create_engine
 
 from econuy.retrieval import cpi
+from econuy.utils import sqlutil
 try:
     from tests.test_session import remove_clutter
 except ImportError:
@@ -13,6 +14,7 @@ except ImportError:
 CUR_DIR = path.abspath(path.dirname(__file__))
 TEST_DIR = path.join(path.dirname(CUR_DIR), "test-data")
 TEST_CON = create_engine("sqlite://").connect()
+sqlutil.insert_csvs(con=TEST_CON, directory=TEST_DIR)
 
 
 def test_revise():
@@ -27,3 +29,15 @@ def test_revise():
     with pytest.raises(ValueError):
         cpi.get(update_loc=TEST_DIR, revise_rows="wrong")
     remove_clutter()
+
+
+def test_sqlutil():
+    remove_clutter()
+    sqlutil.read(con=TEST_CON, command='SELECT * FROM nxr_daily')
+    sqlutil.read(con=TEST_CON, table_name="nxr_daily",
+                 start_date="2011-01-14",
+                 end_date="2012-01-15")
+    sqlutil.read(con=TEST_CON, table_name="nxr_daily",
+                 start_date="2011-01-14")
+    sqlutil.read(con=TEST_CON, table_name="nxr_daily",
+                 end_date="2012-01-15")
