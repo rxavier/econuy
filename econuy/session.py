37f32b3dad9cd46d566a54429287c0c516c46704
Rafael Xavier <rxaviermontero@gmail.com>
2020-07-22 12:51:28 -0300

    Reorder Session

--- econuy/session.py
+++ econuy/session.py
@@ -1,4 +1,5 @@
 import logging
+import warnings
 from datetime import date
 from os import PathLike, path, makedirs
 from pathlib import Path
@@ -126,8 +127,8 @@ class Session(object):
 
         Parameters
         ----------
-        dataset : {'cpi', 'nxr_monthly', 'nxr_daily', 'fiscal', 'naccounts', \
-                'labor', 'wages', 'comm_index', 'rxr_custom', 'rxr_official', \
+        dataset : {'cpi', 'nxr_monthly', 'nxr_daily', 'fiscal', \
+                'public_debt', 'naccounts', 'labor', 'wages', 'rxr_official', \
                 'reserves', 'reserves_changes', 'trade'}
             Type of data to download.
         update : bool, default True
@@ -212,6 +213,9 @@ class Session(object):
                                      only_get=self.only_get,
                                      **kwargs)
         elif dataset == "rxr_custom" or dataset == "rxr-custom":
+            warnings.warn("'rxr_custom' will only be accesible through the "
+                          "'get_custom' method in a future version",
+                          FutureWarning)
             output = rxr.get_custom(update_loc=update_loc,
                                     revise_rows=self.revise_rows,
                                     save_loc=save_loc,
@@ -224,6 +228,9 @@ class Session(object):
                                       only_get=self.only_get,
                                       **kwargs)
         elif dataset == "commodity_index" or dataset == "comm_index":
+            warnings.warn("'commodity_index' will only be accesible through "
+                          "the 'get_custom' method in a future version",
+                          FutureWarning)
             output = commodity_index.get(update_loc=update_loc,
                                          save_loc=save_loc,
                                          only_get=self.only_get,
@@ -263,7 +270,111 @@ class Session(object):
         Parameters
         ----------
         dataset : {'cpi_measures', 'fiscal', 'labor', 'real_wages', \
-                'net_trade', 'tot'}
+                'net_trade', 'tot', 'net_public_debt', 'commodity_index', \
+                'rxr_custom'}
+            Type of data to download.
+        update : bool, default True
+            Whether to update an existing dataset.
+        save : bool, default  True
+            Whether to save the dataset.
+        **kwargs
+            Keyword arguments.
+
+        Returns
+        -------
+        :class:`~econuy.session.Session`
+            Loads the downloaded dataframe into the :attr:`dataset` attribute.
+
+        Raises
+        ------
+        ValueError
+            If an invalid string is given to the ``dataset`` argument.
+
+        """
+        warnings.warn("This method will be renamed 'get_custom' in a "
+                      "future version", FutureWarning)
+
+        if update is True:
+            if isinstance(self.location, (str, PathLike)):
+                update_loc = Path(self.location)
+            else:
+                update_loc = self.location
+        else:
+            update_loc = None
+        if save is True:
+            if isinstance(self.location, (str, PathLike)):
+                save_loc = Path(self.location)
+            else:
+                save_loc = self.location
+        else:
+            save_loc = None
+
+        if dataset == "cpi_measures" or dataset == "price_measures":
+            output = frequent.cpi_measures(update_loc=update_loc,
+                                           save_loc=save_loc,
+                                           only_get=self.only_get,
+                                           **kwargs)
+        elif dataset == "fiscal":
+            output = frequent.fiscal(update_loc=update_loc,
+                                     save_loc=save_loc,
+                                     only_get=self.only_get,
+                                     **kwargs)
+        elif dataset == "net_public_debt":
+            output = frequent.net_public_debt(update_loc=update_loc,
+                                              save_loc=save_loc,
+                                              only_get=self.only_get,
+                                              **kwargs)
+        elif dataset == "labor" or dataset == "labour":
+            output = frequent.labor_rate_people(update_loc=update_loc,
+                                                save_loc=save_loc,
+                                                only_get=self.only_get,
+                                                **kwargs)
+        elif dataset == "wages" or dataset == "real_wages":
+            output = frequent.labor_real_wages(update_loc=update_loc,
+                                               save_loc=save_loc,
+                                               only_get=self.only_get,
+                                               **kwargs)
+        elif dataset == "net_trade":
+            output = frequent.trade_balance(update_loc=update_loc,
+                                            save_loc=save_loc,
+                                            only_get=self.only_get,
+                                            **kwargs)
+        elif dataset == "tot" or dataset == "terms_of_trade":
+            output = frequent.terms_of_trade(update_loc=update_loc,
+                                             save_loc=save_loc,
+                                             only_get=self.only_get,
+                                             **kwargs)
+        elif dataset == "rxr_custom" or dataset == "rxr-custom":
+            output = rxr.get_custom(update_loc=update_loc,
+                                    save_loc=save_loc,
+                                    only_get=self.only_get,
+                                    **kwargs)
+        elif dataset == "commodity_index" or dataset == "comm_index":
+            output = commodity_index.get(update_loc=update_loc,
+                                         save_loc=save_loc,
+                                         only_get=self.only_get,
+                                         **kwargs)
+        else:
+            raise ValueError("Invalid keyword for 'dataset' parameter.")
+
+        self.dataset = output
+        self.logger.info(f"Retrieved '{dataset}' dataset.")
+
+        return self
+
+    def get_custom(self,
+                   dataset: str,
+                   update: bool = True,
+                   save: bool = True,
+                   **kwargs):
+        """
+        Get frequently used datasets.
+
+        Parameters
+        ----------
+        dataset : {'cpi_measures', 'fiscal', 'labor', 'real_wages', \
+                'net_trade', 'tot', 'net_public_debt', 'commodity_index', \
+                'rxr_custom'}
             Type of data to download.
         update : bool, default True
             Whether to update an existing dataset.
@@ -333,6 +444,16 @@ class Session(object):
                                              save_loc=save_loc,
                                              only_get=self.only_get,
                                              **kwargs)
+        elif dataset == "rxr_custom" or dataset == "rxr-custom":
+            output = rxr.get_custom(update_loc=update_loc,
+                                    save_loc=save_loc,
+                                    only_get=self.only_get,
+                                    **kwargs)
+        elif dataset == "commodity_index" or dataset == "comm_index":
+            output = commodity_index.get(update_loc=update_loc,
+                                         save_loc=save_loc,
+                                         only_get=self.only_get,
+                                         **kwargs)
         else:
             raise ValueError("Invalid keyword for 'dataset' parameter.")
 
