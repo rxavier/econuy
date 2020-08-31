5ad05bcca5500d2037a27a6367223429a9ed2cae
Rafael Xavier <rxaviermontero@gmail.com>
2020-08-31 10:55:20 -0300

    Correctly define path for env file

--- econuy/utils/chromedriver.py
+++ econuy/utils/chromedriver.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import chromedriver_autoinstaller
 from dotenv import load_dotenv
@@ -6,6 +7,8 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+
+from econuy.utils import get_project_root
 
 
 def _build():
@@ -15,7 +18,7 @@ def _build():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     try:
-        load_dotenv("econuy/.env")
+        load_dotenv(Path(get_project_root(), "econuy", ".env"))
         service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_PATH")
         return webdriver.Chrome(options=chrome_options, service=service)
