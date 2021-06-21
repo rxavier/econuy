fc2738cd392fcb85996b5c34f106edb05f5584af
Rafael Xavier <rxaviermontero@gmail.com>
2021-06-21 17:01:28 -0300

    Fix retrieval test

--- tests/test_retrieval.py
+++ tests/test_retrieval.py
@@ -1,9 +1,16 @@
+import os
 from pathlib import Path
-
-import pandas as pd
 
 from econuy.session import Session
 from econuy.utils import get_project_root, metadata, ops
+
+def trim_rows(input: str, output: str, rows: int = 10):
+    for f in os.listdir(input):
+        if f.endswith(".csv") and "commodity_weights" not in f:
+            df = ops._read(Path(input, f), file_fmt="csv",
+                           multiindex="included")
+            df = df.reindex(df.index[:-rows])
+            ops._save(df, Path(output, f))
 
 def test_retrieval():
     """Download every available dataset and compare with a previous version.
@@ -11,8 +18,8 @@ def test_retrieval():
     Up to 5% deviations are accepted.
     """
     location = Path(get_project_root().parent, "tests/test-data")
-    s = Session(location=location)
-    s.get_bulk(names="all", save=False)
+    s = Session(location=location, always_save=False)
+    s.get_bulk(names="all")
     for k, v in s.datasets.items():
         print(k)
         test_path = Path(location, f"{k}.csv")
@@ -22,3 +29,4 @@ def test_retrieval():
         div = test / compare
         final = div[((div > 1.05) | (div < 0.95)).any(1)]
         assert len(final) == 0
+
