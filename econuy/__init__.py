8111c27a065a52047ae87b93471d9631126dbc9e
Rafael Xavier <rxaviermontero@gmail.com>
2024-12-30 23:45:58 -0300

    move to uv

--- econuy/__init__.py
+++ econuy/__init__.py
@@ -1,5 +1,3 @@
-from econuy.core import Pipeline
-from econuy.session import Session
 from econuy.retrieval.load import load_dataset
 
-__all__ = ["Pipeline", "Session", "load_dataset"]
+__all__ = ["load_dataset"]
